6e2dda49d082f799d9c3428baedd18374f5b9540
saifgehlot0 <gehlotsaif07@gmail.com>
2022-07-12 23:46:29 +0530

    home page

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// Selection
+
+const navLinksActivation = document.querySelectorAll("#active");
+
+// Navigation Link Activation on click
+
+const changeActiveItem = () => {
+  navLinksActivation.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+navLinksActivation.forEach((link) =>
+  link.addEventListener("click", function () {
+    changeActiveItem();
+    link.classList.add("active");
+  })
+);
